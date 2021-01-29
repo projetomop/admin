e214a27b52531bb806e702f3a8a805a7a7425ef9
herbetjr <herbetjr@gmail.com>
2021-01-29 14:06:37 -0300

    dkjsahdkja

--- resources/views/vendor/adminlte/auth/auth-page.blade.php
+++ resources/views/vendor/adminlte/auth/auth-page.blade.php
@@ -13,7 +13,7 @@
     @yield('css')
 @stop
 
-@section('classes_body'){{ ($auth_type ?? 'login') . '-page' }}@stop
+@section('classes_body'){{ ($auth_type ?? 'login') . '-page bg-white' }}@stop
 
 @section('body')
     <div class="{{ $auth_type ?? 'login' }}-box">
@@ -21,8 +21,8 @@
         {{-- Logo --}}
         <div class="{{ $auth_type ?? 'login' }}-logo">
             <a href="{{ $dashboard_url }}">
-                <img src="{{ asset(config('adminlte.logo_img')) }}" height="50">
-                {!! config('adminlte.logo', '<b>Admin</b>LTE') !!}
+                <img src="{{ asset(config('adminlte.logo_img')) }}" height="200">
+                {{-- {!! config('adminlte.logo', '<b>Admin</b>LTE') !!} --}}
             </a>
         </div>
 
