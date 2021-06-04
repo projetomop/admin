70f3b4c305777c08fca4e7478b29ff31342f7863
gil-gomes <gilanderson98@outlook.com>
2021-06-04 12:48:35 -0300

    auth 12:48

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -11,7 +11,6 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Validation\ValidationException;
 
-
 class AuthController extends Controller
 {
 
--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,8 +2,7 @@
 
 namespace App\Http;
 
-use Illuminate\Foundation\Http\Kernel as HttpKernel;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;;
 
 class Kernel extends HttpKernel
 {
