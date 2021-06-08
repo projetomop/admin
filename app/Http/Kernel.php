d9ac13d211c054e0f4f5764d3d0a2685a5ca8185
gil-gomes <gilanderson98@outlook.com>
2021-06-08 11:03:49 -0300

    auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,7 @@
 
 namespace App\Http;
 
-use Illuminate\Foundation\Http\Kernel as HttpKernel;;
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
 {
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
         'auth/login',
-        'sanctum/token'
+        'https://mop.atenaagencia.com/api/sanctum/token'
     ];
 }
